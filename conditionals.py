288e56332ab43d0cdaeace9c62845ed2795a4887
Syeda Maham Mohsin <157967000+MahamMohsin@users.noreply.github.com>
2025-01-15 02:56:41 +0500

    Add files via upload

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+age=int(input("Enter your age: "))
+
+if(age>=18):
+    print("You are allowed to drive!")
+    
+elif(age<0):#same as else if
+    print("Enter valid age!")
+    
+else:
+    print("You cant drive")
+    #identation tells you are within that code block
+
+print("End of program")#after else
+
+#above is IF-ELIF-ELSE LADDER
+
+#relational/comparision expressions
+# >= == <=
+
+#logical operators
+# and or not
