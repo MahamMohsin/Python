5e0ad0ecc5f78175458f91b808eb56c924a0a42f
Syeda Maham Mohsin <157967000+MahamMohsin@users.noreply.github.com>
2025-01-15 02:49:53 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,15 @@
+#kinda siilar to indexing
+
+#no need to define variable i in python
+#should print from 0-5
+#for(int i=0; i<6; i++)
+#same as for i in range(6)
+for i in range(0,6):
+    print(i)
+    
+print("\nWHILE LOOP")
+#while loop only runs when condition true
+i=0
+while(i<6):
+    print(i)
+    i+=1 #i++ cant be used
